f7804fd21e38d585f173334f5ee650082f7ee090
Simen Haugerud Granlund <simen@hgranlund.com>
2023-03-25 10:40:41 +0100

    Optimizong performance and readability. And updated some packages

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { Transform, TransformCallback, TransformOptions } from 'stream';
 
 export class BinarySplit extends Transform {
   splitOn: Buffer;
-  buffered = [] as Buffer[];
-  pending = [] as Buffer[];
-  constructor (splitOn = '\n' as string | Buffer, opt?: TransformOptions) {
+  buffered: Buffer;
+  constructor(splitOn = '\n' as string | Buffer, opt?: TransformOptions) {
     super(opt);
     if (typeof splitOn === 'string') {
       this.splitOn = Buffer.from(splitOn);
@@ -13,48 +12,29 @@ export class BinarySplit extends Transform {
     }
   }
 
-  getBuffered (): Buffer {
-    if (this.buffered.length === 1) {
-      return this.buffered.pop();
-    } else {
-      const buf = Buffer.concat(this.buffered);
-      this.buffered = [];
-      return buf;
-    }
-  }
-  getPending (): Buffer {
-    return Buffer.concat(this.pending)
-  }
+  _transform(chunk: Buffer, encoding: string, done: TransformCallback): void {
+    this.addToBuffered(chunk);
 
-  _transform (chunk: Buffer, encoding: string, done: TransformCallback): void {
-    let offset = 0;
-    let splitAt = 0;
-    let found = 0;
-    this.pending.push(chunk);
-    while (offset < this.getPending().length) {
-      splitAt = this.getPending().indexOf(this.splitOn, offset);
-      if (splitAt === -1) {
-        this.buffered.push(this.getPending().slice(offset));
-        offset = this.getPending().length;
-      } else {
-        this.push(this.getPending().slice(offset, splitAt));
-        this.buffered = []
-
-        offset = splitAt + this.splitOn.length;
-       found = offset 
-      }
-    }
-    if (found > 0) { 
-      const pending = this.getPending().slice(found)
-      this.pending = [] 
-      this.pending.push(pending)
+    let splitAt = this.buffered.indexOf(this.splitOn);
+    while (splitAt > -1) {
+      this.push(this.buffered.subarray(0, splitAt));
+      this.buffered = this.buffered.subarray(splitAt + this.splitOn.length)
+      splitAt = this.buffered.indexOf(this.splitOn);
     }
     done();
   }
 
-  _flush (done: TransformCallback): void {
-    if (this.buffered.length) {
-      this.push(this.getBuffered());
+  private addToBuffered(chunk: Buffer) {
+    if (this.buffered == null) {
+      this.buffered = chunk;
+    } else {
+      this.buffered = Buffer.concat([this.buffered, chunk]);
+    }
+  }
+
+  _flush(done: TransformCallback): void {
+    if (this.buffered != null) {
+      this.push(this.buffered);
     }
     done();
   }
